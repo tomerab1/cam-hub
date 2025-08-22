84e2273840900614895ef86d31fa2e796b3a0d8b
tomerab <tomer1452002@gmail.com>
2025-08-22 13:46:59 +0300

    refactor/Edited compose.yml and add infra dir

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import WebRTCPlayer from "./components/player/WebRTCPlayer";
 
 export default function App() {
-	const whepUrl = "http://localhost:8889/test_cam/whep";
+	const whepUrl = "http://localhost:8889/cam_01/whep";
 
 	return <WebRTCPlayer whepUrl={whepUrl} />;
 }
