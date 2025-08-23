069f83e6e3e8dcf11cc200010b030512ae1aa3c9
tomerab <tomer1452002@gmail.com>
2025-08-23 23:31:02 +0300

    feat: Added first migrations, postgre image, pgxpool and some basic ui page for camera discovery

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,9 @@
 import WebRTCPlayer from "./components/player/WebRTCPlayer";
+import CameraDiscoveryPage from "./pages/CameraDiscoveryPage";
 
 export default function App() {
 	const whepUrl = "http://localhost:8889/cam_01/whep";
 
-	return <WebRTCPlayer whepUrl={whepUrl} />;
+	// return <WebRTCPlayer whepUrl={whepUrl} />;
+	return <CameraDiscoveryPage />;
 }
